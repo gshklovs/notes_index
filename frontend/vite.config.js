680eb8851132506fba55cfdcda91ba3cb2b53448
Gregory Shklovski <gshklovs@uci.edu>
2025-06-04 18:12:21 -0700

    added test frontend files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
-    exclude: [...configDefaults.exclude, '**/old_tests/**'],
-    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)']
+    exclude: [...configDefaults.exclude, '**/old_tests/**']
   }
 })
